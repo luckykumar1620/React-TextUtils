8dacdb14a86ddf59112a91687f805d8b79edda82
Lucky Kumar <lucky9110132241@gmail.com>
2025-08-24 01:38:07 +0530

    Initial commit - React TextUtils Project

---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+//import { Link } from 'react-router-dom'
+
+
+export default function Navbar({
+      title= 'Set title here',
+       aboutText= "About",
+      mode="",
+      toggleMode=""
+}) 
+{
+  return (
+ 
+      <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}`}>
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">{title}</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
+           <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        {/* <li className="nav-item">
+          <Link className="nav-link" to="/about">{aboutText}</Link>
+        </li> */}
+      </ul>
+      {/* <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-primary" type="submit">Search</button>
+      </form> */}
+      <div className={`form-check form-switch text-${mode==='light'?'dark':'light' }`}>
+       <input className="form-check-input" onClick={toggleMode} type="checkbox" role="switch" id="switchCheckDefault"/>
+      <label className="form-check-label" htmlFor="switchCheckDefault">Enable Darkmode</label>
+      </div>
+    </div>
+  </div>
+</nav>
+   
+  )
+}
+
+Navbar.propTypes = {
+    title:PropTypes.string.isRequired,
+    aboutText:PropTypes.string.isRequired
+}
+
+// Navbar.defaultProps = {
+//   title: "Set title here",
+//   aboutText: "Set aboutText here",
+// //  mode: "light"   // default agar kuch pass na karo
+// };
